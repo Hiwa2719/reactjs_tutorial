93e0b1c07f7106b69d3c0e1dc3374b7be31b7221
hiwa2719 <hiahmadyan@gmail.com>
2022-04-17 11:31:25 +0430

    using props in functional and class components

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ import Hello from './components/hello'
 function App() {
   return (
     <div className="App">
-        {/*<Greet/>*/}
-        {/*<Welcome/>*/}
-        <Hello />
+        <Greet name={'hiwa'} heroName={'Batman'}><p>this is props</p></Greet>
+        <Greet name={'sara'} heroName={'superman'}>
+            <button>action</button>
+        </Greet>
+        <Greet name={'john'} heroName={'wonder woman'}>
+            <h1>hello hiwa</h1>
+        </Greet>
+        <Welcome name={'hiwa'} heroName={'Batman'}>
+            <p>hey you</p>
+        </Welcome>
+        <Welcome name={'sara'} heroName={'superman'}/>
+        <Welcome name={'john'} heroName={'wonder woman'}/>
+        {/*<Hello />*/}
     </div>
   );
 }
