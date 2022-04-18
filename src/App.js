b624e1986f72896dd4d3e6bc1d905e26f693d52c
hiwa2719 <hiahmadyan@gmail.com>
2022-04-18 21:44:14 +0430

    form handling

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/hello'
-import Message from './components/Message'
-import Counter from './components/counter'
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from './components/EventBind'
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameLlist";
-import Stylesheet from "./components/Stylesheet";
-import Inline from "./components/inline";
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-        <Inline/>
-        {/*<Stylesheet primary={true}/>*/}
-        {/*<NameList/>*/}
-        {/*<UserGreeting/>*/}
-        {/*<ParentComponent/>*/}
-        {/*<EventBind/>*/}
-        {/*<FunctionClick/>*/}
-        {/*<ClassClick/>*/}
-        {/*<Counter/>*/}
-        {/*<Message/>*/}
-        {/*<Greet name={'hiwa'} heroName={'Batman'}><p>this is props</p></Greet>*/}
-        {/*<Greet name={'sara'} heroName={'superman'}>*/}
-        {/*    <button>action</button>*/}
-        {/*</Greet>*/}
-        {/*<Greet name={'john'} heroName={'wonder woman'}/>*/}
-        {/*    <h1>hello hiwa</h1>*/}
-        {/*</Greet>*/}
-        {/*<Welcome name={'hiwa'} heroName={'Batman'}>*/}
-        {/*    <p>hey you</p>*/}
-        {/*</Welcome>*/}
-        {/*<Welcome name={'sara'} heroName={'superman'}/>*/}
-        {/*<Welcome name={'john'} heroName={'wonder woman'}/>*/}
-        {/*<Hello />*/}
+        <Form/>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+
+class Form extends React.Component{
+    constructor(props) {
+        super();
+        this.state = {
+            username: '',
+            text: '',
+            topic: 'react',
+        }
+    }
+    handleUsernameChange  = event => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleTextChange = event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleTopicChange = event => {
+        this.setState({
+            topic: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        alert(`${this.state.username} ${this.state.text} ${this.state.topic}`)
+        event.preventDefault()
+    }
+     render(){
+         return(
+             <form onSubmit={this.handleSubmit}>
+                 <div>
+                     <label htmlFor="">Username</label>
+                     <input type="text" value={this.state.username} onChange={this.handleUsernameChange}/>
+                 </div>
+                 <div>
+                     <label htmlFor="">Comments</label>
+                     <textarea name="" id="" cols="30" rows="10" value={this.state.text} onChange={this.handleTextChange}></textarea>
+                 </div>
+                <div>
+                    <label htmlFor="">Topic</label>
+                    <select name="" id="" value={this.state.topic} onChange={this.handleTopicChange}>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+                    </select>
+                </div>
+                 <button type="submit">Submit</button>
+             </form>
+         )
+     }
+}
+
+export default Form
