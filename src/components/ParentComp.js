1cf4f151980f2722fd2f09f8a23c72f5928d4ad8
hiwa2719 <hiahmadyan@gmail.com>
2022-04-19 12:17:13 +0430

    adding memo to functional component

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RegComp from "./RegComp";
 import PureComp from "./PureComp";
+import MemoComp from "./MemoComp";
 
 class ParentComp extends React.Component{
     constructor(props) {
@@ -19,11 +20,13 @@ class ParentComp extends React.Component{
     }
 
     render(){
+        console.log('parent rendering')
         return(
             <div>
                 Parent Component
-                <RegComp name={this.state.name}/>
-                <PureComp name={this.state.name}/>
+                <MemoComp name={this.state.name}/>
+                {/*<RegComp name={this.state.name}/>*/}
+                {/*<PureComp name={this.state.name}/>*/}
             </div>
         )
     }
