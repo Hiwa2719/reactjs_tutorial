473a84ace56c184cfcde7ad3a77473a73ac0d3ca
hiwa2719 <hiahmadyan@gmail.com>
2022-04-20 14:09:55 +0430

    using contextType

--- src/components/E.js
+++ src/components/E.js
@@ -1,10 +1,17 @@
 import React from "react";
 import F from './F'
+import UserContext from "./userContext";
 
 class E extends React.Component{
     render() {
-        return <F/>
+        return (
+            <div>
+                Component E context : {this.context}
+                <F/>
+            </div>)
     }
 }
+
+E.contextType = UserContext
 
 export default E
